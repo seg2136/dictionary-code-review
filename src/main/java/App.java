9fd79dd6e5a96b158334191d0132b8f95b6890c0
Stephany Garcia <seg2136@gmail.com>
2015-08-22 15:42:56 -0700

    Add directories and files for basic framework.

---
+++ src/main/java/App.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.lang.*;
+import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import java.util.Map;
+
+
+public class App {
+
+	public static void main(String[] args) {
+
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
